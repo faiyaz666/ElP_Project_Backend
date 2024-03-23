817087e13eb0c3f1154748a39f2f0106a34db2c9
faiyaz666 <faiyazrafid5@gmail.com>
2024-03-23 16:45:01 +0600

    elp project app

---
+++ src/main/java/com/example/elpprojectbackend/ElpProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.elpprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElpProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElpProjectBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/elpprojectbackend/ElpProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elpprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElpProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
