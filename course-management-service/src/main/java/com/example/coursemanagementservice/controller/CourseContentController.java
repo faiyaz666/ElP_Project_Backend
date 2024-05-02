d070e2f071afe77490503e028beb66c0ae856a03
faiyaz666 <faiyazrafid5@gmail.com>
2024-05-02 16:12:28 +0600

    course content controller created

---
+++ course-management-service/src/main/java/com/example/coursemanagementservice/controller/CourseContentController.java
@@ -0,0 +1,60 @@
+package com.example.coursemanagementservice.controller;
+
+
+import com.example.commonmodule.model.CustomHttpResponse;
+import com.example.commonmodule.util.ResponseBuilder;
+import com.example.coursemanagementservice.model.CourseContent;
+import com.example.coursemanagementservice.service.CourseContentService;
+import jakarta.annotation.Nullable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/course-content")
+public class CourseContentController {
+
+    private final CourseContentService courseContentService;
+
+    public CourseContentController(CourseContentService courseContentService) {
+        this.courseContentService = courseContentService;
+    }
+
+    @GetMapping("/{courseId}")
+    public ResponseEntity<CustomHttpResponse> getCourseContents(@PathVariable UUID courseId,
+                                                                @RequestParam @Nullable Integer pageNumber,
+                                                                @RequestParam @Nullable Integer limit) {
+        List<CourseContent> courseContents;
+        try {
+            courseContents = courseContentService.getAllCourseContents(courseId, pageNumber, limit);
+        } catch (Exception ex) {
+            return ResponseBuilder.buildFailureResponse(HttpStatus.BAD_REQUEST, "400",
+                    "Failed to fetch course contents! Reason: " + ex.getMessage());
+        }
+        return ResponseBuilder.buildSuccessResponse(HttpStatus.OK, Map.of("courseContents", courseContents));
+    }
+
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<CustomHttpResponse> addCourseContent(@RequestBody CourseContent courseContent) {
+        try {
+            courseContentService.addCourseContent(courseContent);
+        } catch (Exception ex) {
+            return ResponseBuilder.buildFailureResponse(HttpStatus.BAD_REQUEST, "400",
+                    "Failed to add course content! Reason: " + ex.getMessage());
+        }
+        return ResponseBuilder.buildSuccessResponse(HttpStatus.CREATED,
+                Map.of("message", "Successfully added course content"));
+    }
+}
